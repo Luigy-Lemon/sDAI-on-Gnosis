35ffc03d5bbbf2c952b1ac6b82814edf6a1e582e
luigy <luigy_lemon@protonmail.com>
2023-09-18 00:45:35 +0100

    quick merge

--- src/BridgeInterestReceiver.sol
+++ src/BridgeInterestReceiver.sol
@@ -4,23 +4,33 @@ pragma solidity ^0.8.20;
 import "openzeppelin/interfaces/IERC4626.sol";
 import "openzeppelin/proxy/utils/Initializable.sol";
 import {IWXDAI} from "./interfaces/IWXDAI.sol";
+import {SavingsXDai} from "./SavingsXDai.sol";
 
 contract BridgeInterestReceiver is Initializable {
+<<<<<<< HEAD
     IWXDAI public immutable wxdai = IWXDAI(0x18c8a7ec7897177E4529065a7E7B0878358B3BfF);
 
+=======
+    IWXDAI public immutable wxdai = IWXDAI(0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d);
+>>>>>>> 074b72e (add vaultAPY function that returns current sDAI vault APY based on current interest being relayed to the receiver and the amount of deposits)
     address public vault;
+    SavingsXDai private sDAI;
 
     uint256 public dripRate;
     uint256 public _nextClaimEpoch;
     uint256 public _lastClaimTimestamp;
     uint256 public epochLength = 30 hours;
+    uint256 private _latestReceivedTimestamp;
+    uint256 public BridgedRate;
 
     event Claimed(uint256 indexed amount);
+
 
     function initialize(address _vault) public payable initializer {
         _nextClaimEpoch = block.timestamp;
         _lastClaimTimestamp = block.timestamp;
         vault = _vault;
+        sDAI = SavingsXDai(payable(_vault));
     }
 
     function claim() external returns (uint256 claimed){
@@ -85,5 +95,20 @@ contract BridgeInterestReceiver is Initializable {
             claimable = unclaimedTime * _dripRate;
         }
         return claimable;
-    }   
+    }
+
+    function vaultAPY() external view returns (uint256){
+        uint256 deposits = sDAI.totalAssets();
+        uint256 bestRate  = (BridgedRate > 0) ? BridgedRate : dripRate;
+        uint256 annualYield = (bestRate * 365 days);
+        return (1 ether * annualYield ) / deposits;
+    }
+
+    receive() external payable{
+        if(msg.sender == address(0)){
+            if (_latestReceivedTimestamp > 0)
+                BridgedRate = msg.value / (block.timestamp - _latestReceivedTimestamp);
+            _latestReceivedTimestamp = block.timestamp;
+        }
+    }
 }
